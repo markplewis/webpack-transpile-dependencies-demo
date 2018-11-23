4e72ae28aa887fd2763767d2b9ac97927fdfd894
markplewis <markplewis1@gmail.com>
2018-11-23 12:28:47 -0500

    Adds JS feature that IE11 doesn't understand

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import phb from 'posthtml-beautify';
+// import phb from 'posthtml-beautify';
 import bf from 'bf';
 
 const text = `lorem
 ipsum ${bf('test')}`;
+
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false
+};
+console.log(Object.values(object1));
